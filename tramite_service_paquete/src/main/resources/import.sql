b993c1248ede7bc221b4a2e293005b613a971451
oheredia <oheredia@exact.com.pe>
2020-03-23 22:26:09 -0500

    Import de paquetes

--- tramite_service_paquete/src/main/resources/import.sql
+++ tramite_service_paquete/src/main/resources/import.sql
@@ -1,2 +1,27 @@
+
+-- IMPORT --
+
 insert into tipo_paquete(nombre, interno) values('SOBRE', 1), ('VALIJA', 1);
-insert into paquete(codigo, en_uso, tipo_paquete_id) values('S000001', 0, 1), ('S000002', 0, 1), ('S000003', 0, 1),  ('S000004', 0, 1);
+
+
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200001',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200002',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200003',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200004',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200005',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200006',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200007',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200008',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200009',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200010',1,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200011',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200012',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200013',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200014',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200015',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200016',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200017',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200018',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200019',0,1);
+INSERT INTO paquete(codigo,en_uso,tipo_paquete_id) VALUES('20200020',0,1);
+
